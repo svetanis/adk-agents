2c15411c0ae9064a2429ff4c0003cab234eb2d94
svetanis <svet125@gmail.com>
2026-04-23 19:41:30 -0700

    code workflow

--- src/main/java/com/svetanis/agents/code/CodeWorkflowApp.java
+++ src/main/java/com/svetanis/agents/code/CodeWorkflowApp.java
@@ -3,7 +3,7 @@ package com.svetanis.agents.code;
 import com.google.adk.web.AdkWebServer;
 import com.svetanis.agents.base.AgentConfigsProvider;
 
-// mvn compile exec:java -Dexec.mainClass=com.svetanis.agents.code.CodePolyglotApp
+// mvn compile exec:java -Dexec.mainClass=com.svetanis.agents.code.CodeWorkflowApp
 
 public final class CodeWorkflowApp {
 
